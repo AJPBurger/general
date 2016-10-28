f91cdb42b0caa6ce2f3db2f5e304696b3163e3bf
aburger <aburger@mercantile.co.za>
2016-10-28 09:45:02 +0200

    Basic coding to check on ODS Report generation create dates

---
+++ ODS_RETAIL_LIST2CSV.ps1
@@ -0,0 +1,14 @@
+﻿Get-ChildItem -Path O:\ODS_RETAIL -Recurse |`
+foreach{
+$Item = $_
+$Type = $_.Extension
+$Path = $_.FullName
+$Folder = $_.PSIsContainer
+$Age = $_.CreationTime
+
+$Path | Select-Object `
+    @{n="Name";e={$Item}},`
+    @{n="Created";e={$Age}},`
+    @{n="filePath";e={$Path}},`
+    @{n="Extension";e={if($Folder){"Folder"}else{$Type}}}`
+}| Export-Csv O:\ODS_RETAIL\Results.csv -NoTypeInformation 
